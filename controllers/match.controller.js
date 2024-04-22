ab2a3c47c5a08b02bd9aedd2db080d5d83db13a9
Korostelev Konstantin <korostel97@gmail.com>
2024-04-23 02:06:58 +0400

    remove sort get matches after

--- controllers/match.controller.js
+++ controllers/match.controller.js
@@ -3,11 +3,9 @@ const Match = require('../models/match.model');
 const getMatchesAfter = async (req, res) => {
   try {
     const date = new Date(parseInt(req.params.date));
-    const matches = await Match.find({ end_time: { $gt: date.getTime() } })
-      .select('-_id')
-      .sort({
-        end_time: 1,
-      });
+    const matches = await Match.find({ end_time: { $gt: date.getTime() } }).select(
+      '-_id',
+    );
     res.status(200).json(matches);
   } catch (error) {
     res.status(500).json({ message: error.message });
